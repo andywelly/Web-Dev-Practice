927aefd8e3038b6c40bc5db24fa53568752c72fc
Andwele Ancheta <andwele.ancheta@gmail.com>
2024-03-04 15:57:36 +1100

    Changed bootstrap card content

---
+++ jQuery/index.js
@@ -0,0 +1,13 @@
+
+$("button").on("click", function () {
+    $("h1").animate({
+        opacity: 0.5
+    });
+  });
+
+
+
+
+$("input").on("keypress", function (event) {
+    console.log(event.key);
+  });
